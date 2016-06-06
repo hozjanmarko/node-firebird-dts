436f1a99f77cff80c282db405357e1ee9fa25e8a
Marko Hozjan <MarkoHozjan@CADIS.local>
2016-06-06 13:17:14 +0200

    initial commit

---
+++ node-firebird.d.ts
@@ -0,0 +1,39 @@
+declare module "node-firebird" {
+
+    export const
+        ISOLATION_READ_UNCOMMITTED: number,
+        ISOLATION_READ_COMMITED: number,
+        ISOLATION_REPEATABLE_READ: number,
+        ISOLATION_SERIALIZABLE: number,
+        ISOLATION_READ_COMMITED_READ_ONLY: number;
+
+    export interface IOptions {
+        host: string,
+        port?: number,
+        database: string,
+        user: string,
+        password: string,
+    }
+
+    export interface IDB {
+        query(query: string, params: Array<any>, Callback: (err: Error, result?: Array<any>) => void): void;
+        execute(query: string, params: Array<any>, Callback: (err: Error, result?: Array<Array<any>>) => void): void;
+        sequentially(query: string, params: Array<any>, ItemFunction: (row: number, index: number) => void, ErrorCallback: (error: Error) => void)
+        detach(Callback: (err: Error) => void): void;
+    }
+
+    export interface ITransaction {
+        query(query: string, params: Array<any>, Callback: (err: Error, result?: Array<any>) => void): void;
+        execute(query: string, params: Array<any>, Callback: (err: Error, result?: Array<Array<any>>) => void): void;
+        commit(Callback: (err: Error) => void): void;
+        rollback(Callback: (err: Error) => void): void;
+    }
+
+    function escape(value: any): string;
+    function attach(IOptions, callback?: (error: Error, db: IDB) => void);
+    function create(IOptions, callback?: (error: Error, db: IDB) => void);
+    function attachOrCreate(IOptions, callback?: (error: Error, db: IDB) => void);
+    function pool(max: number, IOptions, callback?: (error: Error, db: IDB) => void);
+    function transaction(isolation, Callback: (err: Error, transaction: ITransaction) => void);
+    function on(event : string,EventEmitter : ()=>void);
+}
