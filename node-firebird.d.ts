2d7c777c7169d37ca353da7c464f7764b6274856
Marko Hozjan <MarkoHozjan@CADIS.local>
2016-06-07 08:47:31 +0200

    pool

--- node-firebird.d.ts
+++ node-firebird.d.ts
@@ -13,9 +13,9 @@ declare module "node-firebird" {
         database?: string,
         user: string,
         password: string,
-        role? : string,
-        filename? : string,
-        pageSize? : number
+        role?: string,
+        filename?: string,
+        pageSize?: number
     }
 
     export interface IDB {
@@ -24,7 +24,9 @@ declare module "node-firebird" {
         sequentially(query: string, params: Array<any>, ItemFunction: (row: number, index: number) => void, ErrorCallback: (error: Error) => void)
         detach(Callback: (err: Error) => void): void;
     }
-
+    export interface IPool {
+        get(callback: (error: Error, db: IDB) => void);
+    }
     export interface ITransaction {
         query(query: string, params: Array<any>, Callback: (err: Error, result?: Array<any>) => void): void;
         execute(query: string, params: Array<any>, Callback: (err: Error, result?: Array<Array<any>>) => void): void;
@@ -36,7 +38,9 @@ declare module "node-firebird" {
     function attach(IOptions, callback?: (error: Error, db: IDB) => void);
     function create(IOptions, callback?: (error: Error, db: IDB) => void);
     function attachOrCreate(IOptions, callback?: (error: Error, db: IDB) => void);
-    function pool(max: number, IOptions, callback?: (error: Error, db: IDB) => void);
+    function pool(max: number, IOptions): IPool;
     function transaction(isolation, Callback: (err: Error, transaction: ITransaction) => void);
+
+
     //function on(event : string,EventEmitter : ()=>void);
 }
